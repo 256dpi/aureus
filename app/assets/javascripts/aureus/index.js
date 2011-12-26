d7cd583318c2201fb59f392dac2b41b8071e8902
Joël Gähwiler <joel.gaehwiler@bluewin.ch>
2011-12-26 13:12:37 +0100

    corrected pagination bug

--- app/assets/javascripts/aureus/index.js
+++ app/assets/javascripts/aureus/index.js
@@ -37,6 +37,7 @@ $(document).ready(function(){
     $(this).dataTable({
       sDom: 't',
       oLanguage: datatablesLanguageConfig,
+      bPaginate: false,
       aoColumns: datatablesColumnConf($(this))
     });
   });
