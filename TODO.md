839659703efe46a72b5b2ad8856de757bee57599
Joël Gähwiler <joel.gaehwiler@bluewin.ch>
2012-07-20 12:13:41 +0200

    bugfix namspaces

--- TODO.md
+++ TODO.md
@@ -1,7 +1,5 @@
 # ToDo
 
-* add generators for views
-* create helper methods to completly generate interface
 * box head buttons
 * datatable improvements
 * proper namespace css and clean up
--- lib/generators/aureus/views/views_generator.rb
+++ lib/generators/aureus/views/views_generator.rb
@@ -9,17 +9,19 @@ module Aureus
 			desc "generates aures views for a existing model"
 			source_root File.expand_path("../templates", __FILE__)
 			argument :resource, :type => :string, :required => true
-			argument :folder, :type => :string, :default => ""
-
+			argument :controller, :type => :string, :required => true
 			def generate
 				model_name = resource.camelize
+				folder = controller.gsub(/Controller$/,"").underscore
 				target = ["app/views",folder].join "/"
+				namespace = folder.split("/").slice(0...-1)
 				if yes? "generate views for '"+model_name+"' to: "+target+"?"
 					model = model_name.constantize
 					columns = model.column_names
+					columns2 = columns - ["id","created_at","updated_at"]
 					directory "views", target
 					real_name = model_name.demodulize
-					route = model_name.underscore.gsub("/","_")
+					route = folder.gsub("/","_").singularize
 					replacements = {
 						"MODEL" => model_name,
 						"TITLE_SINGULAR" => real_name,
@@ -31,7 +33,8 @@ module Aureus
 						"TABLE_HEADS" => columns.collect{ |c| '        - h.text "'+c.titleize+'"' }.join("\n"),
 						"TABLE_CELLS" => columns.collect{ |c| '        - r.cell '+real_name.downcase+'.'+c }.join("\n"),
 						"ENTRIES" => columns.collect{ |c| '        - l.entry "'+c.titleize+'", @'+real_name.downcase+'.'+c }.join("\n"),
-						"INPUTS" => columns.collect{ |c| '        = f.input :'+c }.join("\n")
+						"INPUTS" => columns2.collect{ |c| '        = f.input :'+c }.join("\n"),
+						"FORM_PATH" => namespace.collect{ |n| '"'+n+'"' }.join(",")
 					}
 					Dir[target+"/*.haml"].each do |file|
 						replacements.each do |key,value|
