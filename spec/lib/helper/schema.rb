496feeab3bbcc0219ccf55b957ecffd6795358e5
Joël Gähwiler <joel.gaehwiler@bluewin.ch>
2014-01-13 23:53:33 +0100

    cleaned specs

--- spec/lib/helper/schema.rb
+++ spec/lib/helper/schema.rb
@@ -1,6 +1,6 @@
 ActiveRecord::Schema.define version: 20120717133935 do
-  create_table "resources", force: true do |t|
-    t.string "title"
-    t.string "text"
+  create_table 'resources', force: true do |t|
+    t.string 'title'
+    t.string 'text'
   end
 end
--- spec/lib/spec_helper.rb
+++ spec/lib/spec_helper.rb
@@ -1,25 +1,25 @@
-require "bundler/setup"
+require 'bundler/setup'
 
-require "rails/all"
-require "haml"
+require 'rails/all'
+require 'haml'
 
 Bundler.require :default, :test
 
-Dir[File.expand_path("../../../lib/aureus/components/*.rb", __FILE__)].each{ |file| require file }
+Dir[File.expand_path('../../../lib/aureus/components/*.rb', __FILE__)].each{ |file| require file }
 
 include Aureus::Helper
 
-ActiveRecord::Base.establish_connection adapter: "sqlite3", database: ":memory:"
-load "spec/lib/helper/schema.rb"
-load "spec/lib/helper/resource.rb"
+ActiveRecord::Base.establish_connection adapter: 'sqlite3', database: ':memory:'
+load 'spec/lib/helper/schema.rb'
+load 'spec/lib/helper/resource.rb'
 
 5.times do |i|
-  Resource.create!(id: i, title: "the title", text: "some long text")
+  Resource.create!(id: i, title: 'the title', text: 'some long text')
 end
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
-  config.order = "random"
+  config.order = 'random'
 end
