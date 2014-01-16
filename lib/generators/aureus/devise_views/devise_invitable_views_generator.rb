2b67aa9dab7a50a8e1bf96c30e4a8c5caec62b6d
Pratik Mukerji <pratik@electricfeel.com>
2014-01-16 14:08:48 +0100

    added a generator for the  invitable views

---
+++ lib/generators/aureus/devise_views/devise_invitable_views_generator.rb
@@ -0,0 +1,23 @@
+module Aureus
+
+  module Generators
+
+    class DeviseInvitableViewsGenerator < Rails::Generators::Base
+
+      desc 'generates aureus compliant devise invitable haml views with aureus helpers'
+      source_root File.expand_path('../templates', __FILE__)
+      argument :folder, type: :string, default: 'devise'
+
+      def generate
+        target = "app/views/#{folder}"
+        directory 'devise_invitable', target
+        Dir[target+'/**/*.haml'].each do |file|
+          gsub_file file, '{{{folder}}}', folder, verbose: false
+        end
+      end
+
+    end
+
+  end
+
+end
