def4376568b62b9b95a6ad7d95ca1432b6c1258d
Pratik Mukerji <pratik@electricfeel.com>
2014-01-16 16:45:30 +0100

    last cleanup

--- lib/generators/aureus/devise_invitable_views/devise_invitable_views_generator.rb
+++ lib/generators/aureus/devise_invitable_views/devise_invitable_views_generator.rb
@@ -11,7 +11,7 @@ module Aureus
       def generate
         target = "app/views/#{folder}"
         directory 'devise_invitable', target
-        Dir[target+'/**/*.haml'].each do |file|
+        Dir[target + '/**/*.haml'].each do |file|
           gsub_file file, '{{{folder}}}', folder, verbose: false
         end
       end
