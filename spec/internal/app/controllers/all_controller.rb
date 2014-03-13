1366c784ffce69711104ee0ec3fb56457e78f842
Joël Gähwiler <joel.gaehwiler@bluewin.ch>
2014-03-13 14:51:30 +0100

    finished test setup

--- spec/internal/app/controllers/all_controller.rb
+++ spec/internal/app/controllers/all_controller.rb
@@ -1,6 +1,9 @@
 class AllController < ActionController::Base
 
   def index
+    flash[:notice] = 'Notice'
+    flash[:error] = 'Error'
+    flash[:alert] = 'Alert'
     render :index, layout: 'application'
   end
 
