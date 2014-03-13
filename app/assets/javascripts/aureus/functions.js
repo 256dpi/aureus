9c4390b9aefeb480ed1464fbc4682a62f7fef873
Joël Gähwiler <joel.gaehwiler@bluewin.ch>
2014-03-13 14:11:09 +0100

    simplified javascript

--- app/assets/javascripts/aureus/functions.js
+++ app/assets/javascripts/aureus/functions.js
@@ -4,17 +4,19 @@ var aureus_default_options = {
 
 function aureus_initialize(options) {
   window.aureus = { options: jQuery.extend({}, aureus_default_options, options || {}) };
+  _aureus_remove_messages_after(window.aureus.options.remove_messages_after);
+  _aureus_datatables_decorate();
   _aureus_simple_map();
   _aureus_ipad_compatibility();
 }
 
-function aureus_remove_messages_after(seconds) {
+function _aureus_remove_messages_after(seconds) {
   setTimeout(function() {
     $(".aureus-messages p").fadeOut();
   }, seconds*1000);
 }
 
-function aureus_datatables_decorate() {
+function _aureus_datatables_decorate() {
   $.fn.dataTableExt.oStdClasses.sWrapper = "datatable-wrapper";
   $.fn.dataTableExt.oStdClasses.sLength = "datatable-length";
   $.fn.dataTableExt.oStdClasses.sFilter = "datatable-filter";
--- spec/internal/app/assets/javascripts/application.js
+++ spec/internal/app/assets/javascripts/application.js
@@ -1,9 +1,8 @@
 //= require aureus
 //= require_self
-//= require_tree .
 
 $(function(){
-  aureus_initialize({});
-  aureus_remove_messages_after(2);
-  aureus_datatables_decorate();
+  aureus_initialize({
+    remove_messages_after: 2
+  });
 });
