a6e80863add3356171998a7f3e153fba52b6e6bf
Joël Gähwiler <joel.gaehwiler@bluewin.ch>
2014-03-14 12:17:17 +0100

    converted tabs

--- lib/aureus/components/data_table.rb
+++ lib/aureus/components/data_table.rb
@@ -1,26 +1,26 @@
 module Aureus
 
-	module Components
+  module Components
 
-		class DataTable < Renderable
+    class DataTable < Renderable
       include ActionView::Helpers::JavaScriptHelper
 
-			def initialize resource
-				@resource = resource
-				@head = DataTableHead.new
-				@rows = Array.new
-			end
+      def initialize resource
+        @resource = resource
+        @head = DataTableHead.new
+        @rows = Array.new
+      end
 
-			def head
-				yield @head
-			end
+      def head
+        yield @head
+      end
 
-			def row
-				@resource.each do |r|
-					row = DataTableRow.new
-					yield row, r
-					@rows << row
-				end
+      def row
+        @resource.each do |r|
+          row = DataTableRow.new
+          yield row, r
+          @rows << row
+        end
       end
 
       def data
@@ -34,60 +34,60 @@ module Aureus
         }
       end
 
-			def render
-				content_tag 'table', id: @resource.class.name.downcase, class: 'datatable', data: data do
-					compact @head.render, content_tag('tbody',compact_render(*@rows))
-				end
-			end
+      def render
+        content_tag 'table', id: @resource.class.name.downcase, class: 'datatable', data: data do
+          compact @head.render, content_tag('tbody',compact_render(*@rows))
+        end
+      end
 
-		end
+    end
 
-		class DataTableHead < Renderable
+    class DataTableHead < Renderable
 
-			def initialize
-				@columns = Array.new
-			end
+      def initialize
+        @columns = Array.new
+      end
 
-			def text name
-				@columns << DataTableHeadColumn.new(name,'text-sorting')
-			end
+      def text name
+        @columns << DataTableHeadColumn.new(name,'text-sorting')
+      end
 
-			def date name
-				@columns << DataTableHeadColumn.new(name,'date-sorting')
-			end
+      def date name
+        @columns << DataTableHeadColumn.new(name,'date-sorting')
+      end
 
-			def raw name
-				@columns << DataTableHeadColumn.new(name,'no-sorting')
-			end
+      def raw name
+        @columns << DataTableHeadColumn.new(name,'no-sorting')
+      end
 
-			def render
-				raw ''
-				content_tag 'thead' do
-					content_tag 'tr', compact_render(*@columns)
-				end
-			end
+      def render
+        raw ''
+        content_tag 'thead' do
+          content_tag 'tr', compact_render(*@columns)
+        end
+      end
 
-		end
+    end
 
-		class DataTableHeadColumn < Renderable
+    class DataTableHeadColumn < Renderable
 
-			def initialize name, sorting
-				@name = name
-				@sorting = sorting
-			end
+      def initialize name, sorting
+        @name = name
+        @sorting = sorting
+      end
 
-			def render
-				content_tag 'th', @name, class: @sorting
-			end
+      def render
+        content_tag 'th', @name, class: @sorting
+      end
 
-		end
+    end
 
-		class DataTableRow < Renderable
+    class DataTableRow < Renderable
 
-			def initialize
-				init_haml_helpers
-				@cells = Array.new
-				@buttons = Array.new
+      def initialize
+        init_haml_helpers
+        @cells = Array.new
+        @buttons = Array.new
         @identifier = ''
       end
 
@@ -95,75 +95,75 @@ module Aureus
         @identifier = value
       end
 
-			def cell data='', &block
-				if block_given?
-					@cells << DataTableRowCell.new(capture_haml(&block))
-				else
-					@cells << DataTableRowCell.new(data)
-				end
-			end
+      def cell data='', &block
+        if block_given?
+          @cells << DataTableRowCell.new(capture_haml(&block))
+        else
+          @cells << DataTableRowCell.new(data)
+        end
+      end
 
-			def button type_or_title, url, *args
-				if type_or_title.is_a? Symbol
-					@buttons << DataTableRowButton.new(type_or_title,type_or_title.to_s,url,args)
-				else
-					@buttons << DataTableRowButton.new(:text,type_or_title.to_s,url,args)
-				end
-			end
+      def button type_or_title, url, *args
+        if type_or_title.is_a? Symbol
+          @buttons << DataTableRowButton.new(type_or_title,type_or_title.to_s,url,args)
+        else
+          @buttons << DataTableRowButton.new(:text,type_or_title.to_s,url,args)
+        end
+      end
 
-			def button_raw content
-				@buttons << Renderable.new(content)
-			end
+      def button_raw content
+        @buttons << Renderable.new(content)
+      end
 
-			def render
-				content_tag 'tr', compact_render(*@cells)+content_tag('td',compact_render(*@buttons),class: 'buttons'), id: @identifier
-			end
+      def render
+        content_tag 'tr', compact_render(*@cells)+content_tag('td',compact_render(*@buttons),class: 'buttons'), id: @identifier
+      end
 
-		end
+    end
 
-		class DataTableRowCell < Renderable
+    class DataTableRowCell < Renderable
 
-			def initialize data
-				@data = data
-			end
+      def initialize data
+        @data = data
+      end
 
-			def render
-				content_tag 'td', @data
-			end
+      def render
+        content_tag 'td', @data
+      end
 
-		end
+    end
 
-		class DataTableRowButton < Renderable
+    class DataTableRowButton < Renderable
 
-			def initialize type, text, url, options
-				init options, remote: true, confirm: 'Delete resource?'
-				@type = type
-				@text = text
-				@url = url
-			end
+      def initialize type, text, url, options
+        init options, remote: true, confirm: 'Delete resource?'
+        @type = type
+        @text = text
+        @url = url
+      end
 
-			def render
-				@text = '&nbsp;' unless @type == :text
-				case @type
-				when :text
-					link_to @text, @url
-				when :print
-					link_to @text, @url, class: :print
-				when :show
-					link_to @text, @url, class: :show
-				when :edit	
-					link_to @text, @url, class: :edit
-				when :destroy
-					if @options[:remote]
-						link_to @text, @url, class: :destroy, method: :delete, data: { confirm: @options[:confirm] }
-					else
-						link_to @text, @url, class: :destroy
-					end
-				end
-			end
+      def render
+        @text = '&nbsp;' unless @type == :text
+        case @type
+        when :text
+          link_to @text, @url
+        when :print
+          link_to @text, @url, class: :print
+        when :show
+          link_to @text, @url, class: :show
+        when :edit  
+          link_to @text, @url, class: :edit
+        when :destroy
+          if @options[:remote]
+            link_to @text, @url, class: :destroy, method: :delete, data: { confirm: @options[:confirm] }
+          else
+            link_to @text, @url, class: :destroy
+          end
+        end
+      end
 
-		end
+    end
 
-	end
+  end
 
 end
--- lib/aureus/helper.rb
+++ lib/aureus/helper.rb
@@ -1,62 +1,62 @@
 module Aureus
 
-	module Helper
+  module Helper
 
-		def aureus_toolbar title
+    def aureus_toolbar title
       toolbar = Components::Toolbar.new title
       yield toolbar
       toolbar.render
-		end
+    end
 
-		def aureus_navigation
-			navigation = Components::Navigation.new
-			yield navigation
-			navigation.render
-		end
+    def aureus_navigation
+      navigation = Components::Navigation.new
+      yield navigation
+      navigation.render
+    end
 
-		def aureus_messages flash
-			messages = Components::Messages.new flash
-			messages.render
-		end
+    def aureus_messages flash
+      messages = Components::Messages.new flash
+      messages.render
+    end
 
-		def aureus_content html
-			content = Components::Content.new html
-			content.render
-		end
+    def aureus_content html
+      content = Components::Content.new html
+      content.render
+    end
 
-		def aureus_row &block
-			row = Components::Row.new &block
-			row.render
-		end
+    def aureus_row &block
+      row = Components::Row.new &block
+      row.render
+    end
 
-		def aureus_box title, *args, &block
-			box = Components::Box.new title, args, &block
-			box.render
-		end
+    def aureus_box title, *args, &block
+      box = Components::Box.new title, args, &block
+      box.render
+    end
 
-		def aureus_datatable resource
-			table = Components::DataTable.new resource
-			yield table
-			table.render
-		end
+    def aureus_datatable resource
+      table = Components::DataTable.new resource
+      yield table
+      table.render
+    end
 
-		def aureus_form args, &block
-			semantic_form_for args do |f|
-				capture_haml f, &block
-			end
-		end
+    def aureus_form args, &block
+      semantic_form_for args do |f|
+        capture_haml f, &block
+      end
+    end
 
-		def aureus_listing
-			listing = Components::Listing.new
-			yield listing
-			listing.render
-		end
+    def aureus_listing
+      listing = Components::Listing.new
+      yield listing
+      listing.render
+    end
 
-		def aureus_simple_map *args
-			map = Components::SimpleMap.new args
-			map.render
-		end
+    def aureus_simple_map *args
+      map = Components::SimpleMap.new args
+      map.render
+    end
 
-	end
+  end
 
 end
