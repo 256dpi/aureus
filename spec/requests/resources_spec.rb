21a6776a133a69791f3b5d159aa4d96a4456d2e5
Joël Gähwiler <joel.gaehwiler@bluewin.ch>
2014-06-15 23:01:12 +0200

    cleanup

--- spec/requests/resources_spec.rb
+++ spec/requests/resources_spec.rb
@@ -1,16 +1,22 @@
 require 'spec_helper'
 
 describe 'Resources' do
-  it 'renders all templates properly' do
+  it 'renders index template properly' do
     get '/resources'
     expect(response).to render_template(:index)
+  end
 
+  it 'renders show template properly' do
     get '/resources/1'
     expect(response).to render_template(:show)
+  end
 
+  it 'renders new template properly' do
     get '/resources/new'
     expect(response).to render_template(:new)
+  end
 
+  it 'renders edit template properly' do
     get '/resources/1/edit'
     expect(response).to render_template(:edit)
   end
