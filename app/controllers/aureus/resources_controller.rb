6e6d8fae149dd1b11995ad8a7e17c92eca381e87
Joël Gähwiler <joel.gaehwiler@bluewin.ch>
2014-06-16 17:56:26 +0200

    do not underscore variable

--- app/controllers/aureus/resources_controller.rb
+++ app/controllers/aureus/resources_controller.rb
@@ -2,54 +2,48 @@ module Aureus
   class ResourcesController < AureusController
     protected
 
-    def aureus_override(hash)
-      @_aureus = (@_aureus || {}).merge(hash)
-    end
-
     def aureus(options)
-      @_aureus ||= {}
-      @_aureus[:box_title] = 'Attributes'
-      @_aureus[:form_inputs] = (options[:form_fields] || []).map{|f| [f] }
+      @aureus ||= {}
+      @aureus[:box_title] = 'Attributes'
+      @aureus[:form_inputs] = (options[:form_fields] || []).map{|f| [f] }
 
-      @_aureus[:resource] = lambda{ instance_variable_get(:"@#{model_name}") }
-      @_aureus[:resources] = lambda{ instance_variable_get(:"@#{model_name.pluralize}") }
+      model_name = params[:controller].split('/').last.singularize
+
+      @aureus[:resource] = lambda{ instance_variable_get(:"@#{model_name}") }
+      @aureus[:resources] = lambda{ instance_variable_get(:"@#{model_name.pluralize}") }
 
       case params[:action]
         when 'index'
-          @_aureus[:title] = "All #{model_name.pluralize.titleize}"
-          @_aureus[:box_title] = 'List'
+          @aureus[:title] = "All #{model_name.pluralize.titleize}"
+          @aureus[:box_title] = 'List'
           if options[:actions].include?(:new)
-            @_aureus[:navigation_buttons] = [["New #{model_name.titleize}", path_for(:new)]]
+            @aureus[:navigation_buttons] = [["New #{model_name.titleize}", path_for(:new)]]
           end
-          @_aureus[:table_cells] = (options[:table_fields] || []).map{|f| [f.to_s.titleize, lambda{|r| r.send(f) }] }
-          @_aureus[:row_actions] = []
+          @aureus[:table_cells] = (options[:table_fields] || []).map{|f| [f.to_s.titleize, lambda{|r| r.send(f) }] }
+          @aureus[:row_actions] = []
           if options[:actions].include?(:show)
-            @_aureus[:row_actions] << lambda{|r| [:show, path_for(:show, r.id)] }
+            @aureus[:row_actions] << lambda{|r| [:show, path_for(:show, r.id)] }
           end
           if options[:actions].include?(:edit)
-            @_aureus[:row_actions] << lambda{|r| [:edit, path_for(:edit, r.id)] }
+            @aureus[:row_actions] << lambda{|r| [:edit, path_for(:edit, r.id)] }
           end
           if options[:actions].include?(:destroy)
-            @_aureus[:row_actions] << lambda{|r| [:destroy, path_for(:destroy, r.id), confirm: 'Really?'] }
+            @aureus[:row_actions] << lambda{|r| [:destroy, path_for(:destroy, r.id), confirm: 'Really?'] }
           end
         when 'new'
-          @_aureus[:title] = "New #{model_name.titleize}"
-            @_aureus[:navigation_buttons] = [['Cancel', path_for(:index)]]
+          @aureus[:title] = "New #{model_name.titleize}"
+            @aureus[:navigation_buttons] = [['Cancel', path_for(:index)]]
         when 'show'
-          @_aureus[:title] = "View #{model_name.titleize}"
-          @_aureus[:navigation_buttons] = [['Back', path_for(:index)]]
-          @_aureus[:item_entries] = (options[:item_fields] || []).map{|f| [f.to_s.titleize, lambda{|r| r.send(f) }] }
+          @aureus[:title] = "View #{model_name.titleize}"
+          @aureus[:navigation_buttons] = [['Back', path_for(:index)]]
+          @aureus[:item_entries] = (options[:item_fields] || []).map{|f| [f.to_s.titleize, lambda{|r| r.send(f) }] }
         when 'edit'
-          @_aureus[:title] = "Edit #{model_name.titleize}"
-          @_aureus[:navigation_buttons] = [['Cancel', path_for(:index)]]
+          @aureus[:title] = "Edit #{model_name.titleize}"
+          @aureus[:navigation_buttons] = [['Cancel', path_for(:index)]]
       end
     end
 
     private
-
-    def model_name
-      params[:controller].split('/').last.singularize
-    end
 
     def path_for(action, id=nil)
       url_for(action: action, id: id, only_path: true)
